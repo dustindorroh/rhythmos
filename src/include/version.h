1675395a1cc0f4f5f34c7e5246f9beaf835bd8d5
Dustin Dorroh <dustindorroh@gmail.com>
2015-05-10 01:20:59 -0700

    Updated version

--- src/include/version.h
+++ src/include/version.h
@@ -8,7 +8,7 @@
 #define VERSION_H
 
 #define VERSION 	"RhythmOS 0.1"
-#define COPYRIGHT 	"Copyright 2011 Dustin Dorroh <dustindorroh@gmail.com>"
+#define COPYRIGHT 	"Copyright 2015 Dustin Dorroh <dustindorroh@gmail.com>"
 #define DISCLAIMER	"\
 This is free software; see the source for copying conditions. There is NO\n\
 warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE."
